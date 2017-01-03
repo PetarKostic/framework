4918eb207e04af5efee7f2e33d72efc62df81e95
PetarKostic <PetarKostic@users.noreply.github.com>
2017-01-03 11:45:30 +0100

    Update PagesController.php

--- framework/controllers/PagesController.php
+++ framework/controllers/PagesController.php
@@ -1,26 +1,25 @@
 <?php 
 
-	class PagesController
-	{
-		public function index()
-		{
-			$brendovi = Brand::all();
-			$kategorije = Kategorija::all();
-			$proizvodi = Proizvod::all();
-			return view('index',compact('brendovi','kategorije','proizvodi'));
-		}
+class PagesController
+{
+   public function index()
+   {
+	$brendovi = Brand::all();
+	$kategorije = Kategorija::all();
+	$proizvodi = Proizvod::all();
+	return view('index',compact('brendovi','kategorije','proizvodi'));
+    }
 
-		public function about()
-		{
-			return view('about');
-		}
+    public function about()
+    {
+	return view('about');
+    }
 
-		public function contact()
-		{
-			return view('contact');
-		}
-		
-	}
+     public function contact()
+     {
+	return view('contact');
+     }
 
+  }
 
  ?>
